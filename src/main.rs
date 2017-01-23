4958b01ceb4d60d463347821180ab759c2f04389
Ben Brittain <ben@okcupid.com>
2017-01-23 12:34:53 -0500

    makes tes mod

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,6 @@ use std::path::Path;
 
 use docopt::Docopt;
 
-mod test;
-mod gen_test;
-//mod encoder;
 mod parser;
 mod codegen;
 mod code_writer;
---
+++ src/test/test.rs
@@ -0,0 +1,12 @@
+extern crate serde;
+extern crate serde_xdr;
+
+mod gen_test;
+
+fn main() {
+
+    let x: gen_test::privilege_groups_t = 42;
+    let vec = serde_xdr::to_bytes(&x);
+    println!("{:?}", vec);
+
+}
