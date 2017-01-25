82b63511b646bec6545ff9adae0848917c9faca1
Brendon Scheinman <brendon@okcupid.com>
2017-01-24 22:17:10 -0500

    added missing file

---
+++ src/function_writer.rs
@@ -0,0 +1,95 @@
+use code_writer::CodeWriter;
+
+pub fn top_decoder<F>(wr: &mut CodeWriter, cb: F)
+            where F : Fn(&mut CodeWriter) {
+    wr.expr_block(
+r###"fn decode(version: u32, proc: u32, buf: &mut EasyBuf) ->
+    Self::Future"###, false, |wr| {
+        wr.write(
+r###"let header_res = serde_xdr::from_bytes<XdrRpcHeader>(buf.to_slice());
+let header = match header_res {
+    Some(h, consumed) => {
+        buf.drain_to(consumed);
+        h
+    }, _ => {
+        return io::Error::new(io::ErrorKind::Other, "failed to read header")
+    }
+}
+"###);
+
+        cb(wr);
+    });
+}
+
+pub fn decoder_miss<S: AsRef<str>>(s: S, wr: &mut CodeWriter) {
+    wr.match_option("_", &Vec::<String>::new(), |wr| {
+        wr.write_line(&format!(
+            "io::Error::new(io::ErrorKind::Other, \"unknown {}\");",
+            s.as_ref()));
+    });
+}
+
+pub fn prog_decoder<S: AsRef<str>, F>(fn_name: S, wr: &mut CodeWriter, cb: F)
+        where F : Fn(&mut CodeWriter) {
+    wr.expr_block(&format!(
+r###"pub fn {}(version: u32, proc: u32, buf: &mut EasyBuf) ->
+    io::Result<Option<Self::In>>"###, fn_name.as_ref()), false, cb);
+}
+
+pub fn prog_decoder_call<S: AsRef<str>>(fn_name: S, wr: &mut CodeWriter) {
+    wr.write_line(&format!("{}(header.version, header.proc, buf);",
+        fn_name.as_ref()));
+}
+
+pub fn version_decoder_match<F>(wr: &mut CodeWriter, cb: F)
+        where F : Fn(&mut CodeWriter) {
+    wr.expr_block("let request = match proc", false, cb);
+}
+
+pub fn version_decoder<S: AsRef<str>, F>(fn_name: S, wr: &mut CodeWriter, cb: F)
+        where F : Fn(&mut CodeWriter) {
+    wr.expr_block(&format!(
+r###"pub fn {}(proc: u32, buf: &mut EasyBuf) ->
+    io::Result<Option<Self::In>>"###, fn_name.as_ref()), false, cb);
+}
+
+pub fn version_decoder_call<S: AsRef<str>>(fn_name: S, wr: &mut CodeWriter) {
+    wr.write_line(&format!("{}(proc, buf);", fn_name.as_ref()));
+}
+
+pub fn proc_decoder<S: AsRef<str>, F>(fn_name: S, wr: &mut CodeWriter, cb: F)
+        where F : Fn(&mut CodeWriter) {
+    wr.expr_block(&format!(
+r###"pub fn {}(buf: &mut EasyBuf) ->
+    io::Result<Option<Self::In>>"###, fn_name.as_ref()), false, cb);
+}
+
+pub fn proc_decoder_call<S: AsRef<str>>(fn_name: S, wr: &mut CodeWriter) {
+    wr.write_line(&format!("{}(buf);", fn_name.as_ref()));
+}
+
+pub fn proc_arg_decoder<S: AsRef<str>>(arg_index: u32, arg_type: S,
+                                       wr: &mut CodeWriter) {
+    wr.write_line(&format!(
+r###"let res{0} = serde_xdr::from_bytes<{1}>(buf.to_slice());
+    let arg{0} = match res{0} {{
+        Some(arg, consumed) => {{
+            buf.drain_to(consumed);
+            arg
+        }},
+        None => {{
+            return Err(io::Error::new(io::ErrorKind::Other,
+                "argument parse failure"));
+        }}
+    }}
+"###, arg_index, arg_type.as_ref()));
+}
+
+pub fn proc_decoder_finalize<S1: AsRef<str>, S2: AsRef<str>>(
+        req_type: S1, req_name: S2, n_args: u32, wr: &mut CodeWriter) {
+    wr.write(&format!("{}::{}(", req_type.as_ref(), req_name.as_ref()));
+
+    let arg_list = (0..n_args).map(|x| { format!("arg{}", x) }).collect();
+    wr.comma_fields(&arg_list);
+    wr.raw_write(");\n");
+}
